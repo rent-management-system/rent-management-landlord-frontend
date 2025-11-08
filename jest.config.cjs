78540ccbef8ce0454df4c17402c3ecaf28810ba9
dagi <dagiteferi2011@gmail.com>
2025-11-08 21:15:53 +0300

    integratios

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,12 +3,12 @@ const { createDefaultPreset } = require("ts-jest");
 const tsJestTransformCfg = createDefaultPreset().transform;
 
 /** @type {import("jest").Config} **/
-export default {
+module.exports = { // Changed from export default
   testEnvironment: "jsdom",
   transform: {
     ...tsJestTransformCfg,
   },
   moduleNameMapper: {
-    '^@/(.*): '<rootDir>/src/$1',
+    '^@/(.*)$': '<rootDir>/src/$1',
   },
 };
