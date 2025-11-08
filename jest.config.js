989486a460a1348b57a6a0b76b54ff41ab34542e
dagi <dagiteferi2011@gmail.com>
2025-11-08 23:49:15 +0300

    Revert "integratios"
    This reverts commit 78540ccbef8ce0454df4c17402c3ecaf28810ba9.

--- jest.config.js
+++ jest.config.js
@@ -3,12 +3,12 @@ const { createDefaultPreset } = require("ts-jest");
 const tsJestTransformCfg = createDefaultPreset().transform;
 
 /** @type {import("jest").Config} **/
-module.exports = { // Changed from export default
+export default {
   testEnvironment: "jsdom",
   transform: {
     ...tsJestTransformCfg,
   },
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@/(.*): '<rootDir>/src/$1',
   },
 };
